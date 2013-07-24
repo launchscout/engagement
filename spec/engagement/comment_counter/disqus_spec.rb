095e6276772c0040ebbe68386688b1b2f8559921
Michael Guterl <michael@diminishing.org>
2013-07-24 11:01:54 -0400

    Skip disqus test unless forum api key is present

--- spec/engagement/comment_counter/disqus_spec.rb
+++ spec/engagement/comment_counter/disqus_spec.rb
@@ -2,10 +2,14 @@ require 'spec_helper'
 
 describe Engagement::CommentCounter::Disqus, :vcr do
 
-  it 'returns the number of comments for the given url' do
-    url = 'http://diminishing.org/searching-for-a-hero-ruby'
-    counter = Engagement::CommentCounter::Disqus.new(disqus_forum_api_key)
-    counter.comments_count(url).should == 4
+  if disqus_forum_api_key
+    it 'returns the number of comments for the given url' do
+      url = 'http://diminishing.org/searching-for-a-hero-ruby'
+      counter = Engagement::CommentCounter::Disqus.new(disqus_forum_api_key)
+      counter.comments_count(url).should == 4
+    end
+  else
+    puts 'Skipping Disqus test, please set the DISQUS_FORUM_API_KEY environment variable.'
   end
 
 end
