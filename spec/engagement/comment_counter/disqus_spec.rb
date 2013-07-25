a1bb6c2933082b6fbbd2c94f31b96edc8518475a
Michael Guterl <michael@diminishing.org>
2013-07-25 10:19:37 -0400

    Disqus should handle threads that we don't own

--- spec/engagement/comment_counter/disqus_spec.rb
+++ spec/engagement/comment_counter/disqus_spec.rb
@@ -1,12 +1,24 @@
 require 'spec_helper'
 
-describe Engagement::CommentCounter::Disqus, :vcr do
+describe Engagement::CommentCounter::Disqus do
 
   if disqus_forum_api_key
-    it 'returns the number of comments for the given url' do
-      url = 'http://diminishing.org/searching-for-a-hero-ruby'
-      counter = Engagement::CommentCounter::Disqus.new(disqus_forum_api_key)
-      counter.comments_count(url).should == 4
+    let(:counter) { Engagement::CommentCounter::Disqus.new(disqus_forum_api_key) }
+
+    context "when we own the url", :vcr do
+      let(:url) { 'http://diminishing.org/searching-for-a-hero-ruby' }
+
+      it 'returns the number of comments for the given url' do
+        counter.comments_count(url).should == 4
+      end
+    end
+
+    context "when we don't own the url", :vcr do
+      let(:url) { 'http://google.com' }
+
+      it "returns zero when we don't own the url" do
+        counter.comments_count(url).should == 0
+      end
     end
   else
     puts 'Skipping Disqus test, please set the DISQUS_FORUM_API_KEY environment variable.'
