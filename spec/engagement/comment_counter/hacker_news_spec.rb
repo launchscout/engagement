2a623dcc001f12886ea46f807c7153d20c89d3e8
Chris Moore <chrism@gaslight.co>
2015-04-06 19:44:11 -0400

    Fix RSpec deprecations

--- spec/engagement/comment_counter/hacker_news_spec.rb
+++ spec/engagement/comment_counter/hacker_news_spec.rb
@@ -5,7 +5,7 @@ describe Engagement::CommentCounter::HackerNews, :vcr do
   it 'returns the number of comments for the given url' do
     url = 'http://www.90percentofeverything.com/2013/07/23/the-slippery-slope/'
     counter = Engagement::CommentCounter::HackerNews.new
-    counter.comments_count(url).should == 18
+    expect(counter.comments_count(url)).to eq(18)
   end
 
 end
--- spec/engagement/comment_counter/twitter_spec.rb
+++ spec/engagement/comment_counter/twitter_spec.rb
@@ -4,10 +4,10 @@ describe Engagement::CommentCounter::Twitter do
 
   if twitter_credentials_present?
     let(:counter) { Engagement::CommentCounter::Twitter.new(twitter_credentials) }
-    let(:url) { 'http://gaslight.co/blog/angular-backed-svgs' }
+    let(:url) { 'https://teamgaslight.com/blog/how-continuous-deployment-boosts-developer-morale-and-productivity' }
 
     it 'returns the number of comments for the given url', :vcr do
-      counter.comments_count(url).should == 15
+      expect(counter.comments_count(url)).to eq(7)
     end
   else
     puts 'Skipping Twitter test, please set the TWITTER_CONSUMER_KEY, TWITTER_CONSUMER_SECRET, TWITTER_OAUTH_TOKEN, TWITTER_OAUTH_TOKEN_SECRET environment variables.'
